042212c9e7688444e3cd5d29c9812e7889d0f2c3
xiangli2341 <djsona520@gmail.com>
2018-09-01 09:50:33 +0800

    添加日志记录和逻辑注释

--- src/main/java/org/ml/accountcenter/web/MlUserController.java
+++ src/main/java/org/ml/accountcenter/web/MlUserController.java
@@ -1,5 +1,6 @@
 package org.ml.accountcenter.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.ml.accountcenter.entity.MlUser;
 import org.ml.accountcenter.service.MlUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
  * @author 
  *
  */
-
+@Slf4j
 @RestController
 public class MlUserController {
 
@@ -33,18 +34,22 @@ public class MlUserController {
     /**
      * demo 根据 手机号查找个人信息
      *   测试接口：http://localhost:1001/findByPhoneNumber?phoneNumber=17829030612
+     *
+     *   num：查找到的数据条数
      * @param phoneNumber
      * @return
      */
     @RequestMapping("/findByPhoneNumber")
     public Integer findByPhoneNumber(@RequestParam String phoneNumber) {
+        int num=service.findByPhoneNumber(phoneNumber);
+        log.info("日志输出 根据手机号查找到："+num+"条数据");
         return service.findByPhoneNumber(phoneNumber);
     }
 
 
     /**
      * demo 根据 手机号查找个人信息
-     *   测试接口：http://localhost:1001/addUser?username=asdli&PASSWORD=123456&STATUS=1&regTime=20181012&phoneNumber=18700523543
+     *   测试接口：http://localhost:1001/addUser?username=asdli&password=123456&status=1&regTime=20181012&phoneNumber=18700523543
      *   添加时先进行手机号检测，有则返回false
      *   无 进行注册
      * @param
@@ -58,23 +63,23 @@ public class MlUserController {
 
             //进行数据库查找
             Integer a=service.findByPhoneNumber(phoneNumber);
+            //如果查找条数为0,则进行添加操作
             if(a==0){
+                //如果添加操作返回true,flag-true,返回添加成功
                 if(service.addUser(mlUser)==true){
                     flag=true;
-                    System.out.println("添加成功！！");
-                    System.out.println("---------------------");
-                }else{
-                    System.out.println("添加失败！！");
-                    System.out.println("---------------------");
+                    log.info("日志输出 =====添加成功！！");
+                }else{                              //否则添加操作返回添加出现异常,添加失败
+                    log.debug("日志输出 =====添加出现异常,失败！！");
                 }
+            }else{
+                log.error("添加失败，数据库已有注册的手机号！！！");
             }
         }catch(Exception e){
             e.printStackTrace();
-            System.out.println("出现异常，添加失败！！");
-            System.out.println("---------------------");
+            log.debug("日志输出 =====添加出现异常,失败！！");
         }
-        System.out.println("结果返回为："+flag);
-        System.out.println("---------------------");
+        log.info("结果返回为："+flag);
         return flag;
     }
 
